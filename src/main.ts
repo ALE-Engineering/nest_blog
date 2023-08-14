5b60eef060c8f3322559fe3e9fda9dced7e7fa9e
= <kilualthaus@hotmail.com>
2023-08-14 22:22:45 +0200

    installation und integration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger-Konfiguration
+  const config = new DocumentBuilder()
+      .setTitle('Nest.js API')
+      .setDescription('API documentation for Nest.js application')
+      .setVersion('1.0')
+      .addTag('nestjs')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
