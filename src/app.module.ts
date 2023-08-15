aa2a6a4ca092f9ab92c4c7a5c57d0f7b0f0332b8
= <kilualthaus@hotmail.com>
2023-08-15 21:26:44 +0200

    delete greeting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { BlogModule } from './blog/blog.module';
 
@@ -19,7 +17,7 @@ import { BlogModule } from './blog/blog.module';
 
     BlogModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
